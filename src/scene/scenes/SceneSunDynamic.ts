cc5af6c4da3433fd031b7cda1c95f8bc2add206d
brocadeyu <yjb1725594062@gmail.com>
2024-09-26 09:46:07 +0800

    feat: sun

--- src/scene/scenes/SceneSunDynamic.ts
+++ src/scene/scenes/SceneSunDynamic.ts
@@ -9,6 +9,8 @@ import { Sky } from 'three/examples/jsm/Addons.js'
  */
 export default class SceneSunDynamic extends SceneBase {
   init() {
+    this.acc = 0.001
+    this.sunElevation = 2.0
     this.orbitControl = new OrbitControls(this.camera, this.renderer.domElement)
     this.camera.position.set(-13.49, 14.19, -58.28)
 
@@ -19,13 +21,14 @@ export default class SceneSunDynamic extends SceneBase {
     const sky = new Sky()
     sky.scale.setScalar(450000)
     this.scene.add(sky)
-    const sun = new THREE.Vector3()
-    const effectController = {
+    this.sky = sky
+    this.sun = new THREE.Vector3()
+    this.effectController = {
       turbidity: 10,
       rayleigh: 3,
       mieCoefficient: 0.005,
       mieDirectionalG: 0.7,
-      elevation: 2,
+      // elevation: 2,
       azimuth: 180,
       exposure: this.renderer.toneMappingExposure
     }
@@ -36,35 +39,61 @@ export default class SceneSunDynamic extends SceneBase {
     const gui = window.guiInstance
 
     const guiChanged = () => {
-      const uniforms = sky.material.uniforms
-      uniforms['turbidity'].value = effectController.turbidity
-      uniforms['rayleigh'].value = effectController.rayleigh
-      uniforms['mieCoefficient'].value = effectController.mieCoefficient
-      uniforms['mieDirectionalG'].value = effectController.mieDirectionalG
+      const uniforms = this.sky.material.uniforms
+      uniforms['turbidity'].value = this.effectController.turbidity
+      uniforms['rayleigh'].value = this.effectController.rayleigh
+      uniforms['mieCoefficient'].value = this.effectController.mieCoefficient
+      uniforms['mieDirectionalG'].value = this.effectController.mieDirectionalG
 
-      const phi = THREE.MathUtils.degToRad(90 - effectController.elevation)
-      const theta = THREE.MathUtils.degToRad(effectController.azimuth)
+      const phi = THREE.MathUtils.degToRad(90 - this.sunElevation)
+      const theta = THREE.MathUtils.degToRad(this.effectController.azimuth)
 
-      sun.setFromSphericalCoords(1, phi, theta)
+      this.sun.setFromSphericalCoords(1, phi, theta)
 
-      uniforms['sunPosition'].value.copy(sun)
+      uniforms['sunPosition'].value.copy(this.sun)
 
-      this.renderer.toneMappingExposure = effectController.exposure
+      this.renderer.toneMappingExposure = this.effectController.exposure
       this.render()
     }
-    gui.add(effectController, 'turbidity', 0.0, 20.0, 0.1).onChange(guiChanged)
-    gui.add(effectController, 'rayleigh', 0.0, 4, 0.001).onChange(guiChanged)
     gui
-      .add(effectController, 'mieCoefficient', 0.0, 0.1, 0.001)
+      .add(this.effectController, 'turbidity', 0.0, 20.0, 0.1)
       .onChange(guiChanged)
     gui
-      .add(effectController, 'mieDirectionalG', 0.0, 1, 0.001)
+      .add(this.effectController, 'rayleigh', 0.0, 4, 0.001)
       .onChange(guiChanged)
-    gui.add(effectController, 'elevation', 0, 90, 0.1).onChange(guiChanged)
-    gui.add(effectController, 'azimuth', -180, 180, 0.1).onChange(guiChanged)
-    gui.add(effectController, 'exposure', 0, 1, 0.0001).onChange(guiChanged)
+    gui
+      .add(this.effectController, 'mieCoefficient', 0.0, 0.1, 0.001)
+      .onChange(guiChanged)
+    gui
+      .add(this.effectController, 'mieDirectionalG', 0.0, 1, 0.001)
+      .onChange(guiChanged)
+    // gui.add(effectController, 'elevation', 0, 90, 0.1).onChange(guiChanged)
+    gui
+      .add(this.effectController, 'azimuth', -180, 180, 0.1)
+      .onChange(guiChanged)
+    gui
+      .add(this.effectController, 'exposure', 0, 1, 0.0001)
+      .onChange(guiChanged)
 
     guiChanged()
+  }
+  update() {
+    // console.log('111')
+    this.sunElevation += this.acc
+    const uniforms = this.sky.material.uniforms
+    const phi = THREE.MathUtils.degToRad(90 - this.sunElevation)
+    const theta = THREE.MathUtils.degToRad(this.effectController.azimuth)
+    this.sun.setFromSphericalCoords(1, phi, theta)
+    uniforms['sunPosition'].value.copy(this.sun)
+    if (this.sunElevation > 180) {
+      this.sunElevation = -10
+    }
+    if (this.sunElevation > 30 && this.acc === 0.001) {
+      this.acc = 0.1
+    }
+    if (this.sunElevation > 150 && this.acc === 0.1) {
+      this.acc = 0.001
+    }
   }
   render() {
     this.orbitControl.update()
